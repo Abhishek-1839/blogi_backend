8951cb887dc3b046301e734a54aace5462f67a81
Abhishek-1839 <abhishektomarsingh7@gmail.com>
2025-03-07 15:51:23 +0530

    commit of server

---
+++ init_db.py
@@ -0,0 +1,8 @@
+from main import Base, engine
+import asyncio
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+asyncio.run(init_db())
