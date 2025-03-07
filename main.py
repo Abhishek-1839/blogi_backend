783414ff2b8200208d96b60cdae57a0ce53eb422
Abhishek-1839 <abhishektomarsingh7@gmail.com>
2025-03-07 16:41:53 +0530

    database

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ from fastapi.middleware.cors import CORSMiddleware
 load_dotenv()
 
 # Use environment variables
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://blogi_o5fm_user:6VTcsl8grKAR802p6T8H7eT0lwsetfl9@dpg-cv5d6b56l47c73d3na50-a/blogi_o5fm")
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
